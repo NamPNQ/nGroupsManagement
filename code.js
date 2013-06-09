b61886fba4f4bff74cbb86ae3afd7ebba50321be
NamPNQ <nampnq@gmail.com>
2013-06-09 11:20:43 +0700

    Update code.js

--- code.js
+++ code.js
@@ -38,9 +38,7 @@ function check_rate(){
         lt_unlike=data.match(/Unlikes \(Reports\)<\/label><\/td><td width=\"20\"><\/td><td>(\d+)<\/td><\/tr>/)[1];
         
         
-        $("div#liketool h2 a").append("<br /><br />Rate:"+parseInt(lt_unlike*100/lt_likesmade)+"% - 
-
-"+parseInt((lt_unlike*100/4)-lt_likesmade));
+        $("div#liketool h2 a").append("<br /><br />Rate:"+parseInt(lt_unlike*100/lt_likesmade)+"% - "+parseInt((lt_unlike*100/4)-lt_likesmade));
     });
 }
 
@@ -49,9 +47,7 @@ $("body").append("<div
 style=\"position:fixed;right:20px;bottom:0px;height:418px;width:380px;display:block;background:#fff;border-top:1px solid 
 
 #000;border-left:1px solid #000;border-right:1px solid #000;border-radius:3px 3px 0px 0px;\" id=\"liketool\"></div>");
-$("div#liketool").html("<h2><a href=\"http://hs2t.com/\">Liketool - Hs2T.com</a></h2><small id=\"countdown\" style=\"color: 
-
-#000;\"></small>");
+$("div#liketool").html("<h2><a href=\"http://hs2t.com/\">Liketool - Hs2T.com</a></h2><small id=\"countdown\" style=\"color:#000;\"></small>");
 checktime();
 check_rate();
 
@@ -65,9 +61,7 @@ function appendAlert(msg){
 }
 var iframe = "<iframe width='468' height='60' frameborder='0' 
 
-src='http://www.ptp22.com/seo.php?username=leeshin456&format=468x60' marginwidth='0' marginheight='0' vspace='0' hspace='0' 
-
-allowtransparency='true' scrolling='no'></iframe>";
+src='http://www.ptp22.com/seo.php?username=leeshin456&format=468x60' marginwidth='0' marginheight='0' vspace='0' hspace='0' allowtransparency='true' scrolling='no'></iframe>";
 //$("div#body center").eq(0).html(iframe);
 
 
